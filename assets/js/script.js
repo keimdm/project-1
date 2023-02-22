32d380875468cdfe8974a17b17c3918695963d1d
David Keim <dmk252@cornell.edu>
2023-02-22 11:32:27 -0500

    added displayResults function

--- assets/js/script.js
+++ assets/js/script.js
@@ -18,7 +18,25 @@ function getCocktails(temperature) {
 }
 
 function displayResults(data) {
-
+    // loop through all entries in cocktail data
+    for (i = 0; i < data.length; i++) {
+        // create elements needed for cocktail recipe display
+        var newCard = $(document.createElement("article"));
+        var newIngredients = $(document.createElement("ul"));
+        var newInstructions = $(document.createElement("p"));  
+        // loop through all ingredient entries and add them to new Ingredients list  
+        for (j = 0; j < data[i].ingredients.length; j++) {
+            newItem = $(document.createElement("li"));
+            newItem.text(data[i].ingredients[j]);
+            newIngredients.append(newItem);
+        }
+        // append elements onto card
+        newInstructions.text(data[i].instructions);
+        newCard.append(newIngredients);
+        newCard.append(newInstructions);
+        // append card onto cocktail list
+        cocktailList.append(newCard);
+    }
 }
 
 // USER INTERACTIONS
