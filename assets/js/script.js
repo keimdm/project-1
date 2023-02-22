d1fa2004f2ed793d56e1f8ef2d1752f552204697
Derek Araujo <derekaraujo@Dereks-MBP.fios-router.home>
2023-02-22 11:27:58 -0500

    add API functioning for cocktail ingredients and weather

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,40 +1,49 @@
 // DEPENDENCIES
 var cocktailList = $("#cocktail-list");
-var name = 'bloody mary'
-$.ajax({
-    method: 'GET',
-    url: 'https://api.api-ninjas.com/v1/cocktail?name=' + name,
-    headers: { 'X-Api-Key': 'FY5H8mVkxpSV+RQ0ub8Cbg==HmezQ5tZdVLtj20h'},
-    contentType: 'application/json',
-    success: function(result) {
-        console.log(result);
-    },
-    error: function ajaxError(jqXHR) {
-        console.error('Error: ', jqXHR.responseText);
-    }
-});
 
-fetch('https://api.openweathermap.org/data/2.5/weather?q=location&appid=32306f990946c772c9c7ada42e2ab47f')
+
+// DATA
+// add NYC latitude and longitude
+var latNYC = '40.7129';
+var lonNYC = '-74.0060';
+var hotSearch = "lemon";
+var coldSearch = "chocolate";
+
+// FUNCTIONS
+function getWeather(lat,lon) {
+    fetch('https://api.openweathermap.org/data/2.5/weather?lat=' + lat + '&lon=' + lon + '&appid=f53b5109b06704799e5260e2dda10bda')
 .then(response => {
     return response.json();
 })
 .then(data => {
     console.log(data);
+    getCocktails(data.main.temp);
 });
-
-// DATA
-// add NYC latitude and longitude
-var latNYC = 0;
-var lonNYC = 0;
-var hotSearch = "lemon";
-var coldSearch = "cream";
-
-// FUNCTIONS
-function getWeather(lat, lon) {
 
 }
 
 function getCocktails(temperature) {
+    var ingredients;
+    if (Number(temperature)>283){
+        ingredients=hotSearch;
+    }
+    else {
+        ingredients=coldSearch;
+    }
+$.ajax({
+    method: 'GET',
+    //Only cocktails containing all listed ingredients will be returned.
+    url: 'https://api.api-ninjas.com/v1/cocktail?ingredients=' + ingredients,
+    headers: { 'X-Api-Key': 'FY5H8mVkxpSV+RQ0ub8Cbg==HmezQ5tZdVLtj20h'},
+    contentType: 'application/json',
+    success: function(result) {
+        console.log(result);
+        displayResults(result);
+    },
+    error: function ajaxError(jqXHR) {
+        console.error('Error: ', jqXHR.responseText);
+    } 
+});
 
 }
 
