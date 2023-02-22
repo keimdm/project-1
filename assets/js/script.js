d349932dad4b84ff60e87513bd275e813de40013
David Keim <dmk252@cornell.edu>
2023-02-21 23:09:14 -0500

    added JS pseudocode

---
+++ assets/js/script.js
@@ -0,0 +1,27 @@
+// DEPENDENCIES
+var cocktailList = $("#cocktail-list");
+
+// DATA
+// add NYC latitude and longitude
+var latNYC = 0;
+var lonNYC = 0;
+var hotSearch = "lemon";
+var coldSearch = "cream";
+
+// FUNCTIONS
+function getWeather(lat, lon) {
+
+}
+
+function getCocktails(temperature) {
+
+}
+
+function displayResults(data) {
+
+}
+
+// USER INTERACTIONS
+
+// INITIALIZATIONS
+getWeather(latNYC, lonNYC);
